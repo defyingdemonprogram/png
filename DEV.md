44af9cd55bd252826e4626977a79a3093d7bc6db
realsanjeev2 <realsanjeev2@gmail.com>
2025-05-11 23:58:21 +0545

    [png.c] Encrypt secret message inside the png image

--- DEV.md
+++ DEV.md
@@ -8,6 +8,56 @@ Every PNG file begins with the same fixed 8-byte signature, represented by the f
 
 This signature identifies the file as a PNG and ensures that the following data is a valid PNG image. The image data is organized into a series of chunks, starting with an `IHDR` chunk and ending with an `IEND` chunk.
 
+### PNG Chunk Naming Convention
+
+In the PNG file format, **chunk types** are identified by **four ASCII letters**, with each letter's **case (uppercase/lowercase)** encoding specific **properties** using **bit 5 (value 32)** of each byte. This design allows decoders and editors to interpret unknown chunks more intelligently.
+
+Each of the 4 characters in the chunk name has a specific bit that conveys the following:
+
+1. **Ancillary bit (1st letter)**
+
+   * **Uppercase (0)** = **Critical** (essential to image decoding)
+   * **Lowercase (1)** = **Ancillary** (optional for decoding)
+
+2. **Private bit (2nd letter)**
+
+   * **Uppercase (0)** = **Public** (standard or registered)
+   * **Lowercase (1)** = **Private** (application-specific)
+
+3. **Reserved bit (3rd letter)**
+
+   * **Must be Uppercase (0)** in current PNG version
+   * **Lowercase (1)** = **Invalid in current standard**
+
+4. **Safe-to-copy bit (4th letter)**
+
+   * **Uppercase (0)** = **Unsafe to copy** if unrecognized
+   * **Lowercase (1)** = **Safe to copy**
+
+For example, chunk name **cHNk**:
+
+* `c` = ancillary (1)
+* `H` = public (0)
+* `N` = reserved bit = 0 (valid)
+* `k` = safe to copy (1)
+  So, **cHNk** is an *ancillary, public, valid, and safe-to-copy* chunk.
+
+
+### Endian Issue in PNG
+
+* PNG uses **big-endian** byte order for encoding integer values. However, most modern architectures such as ARM and x86 use **little-endian** byte order, where the least significant byte is stored first. In contrast, **big-endian** stores the most significant byte first.
+
+  For example, the hexadecimal number `0x12345678` would be stored in memory as:
+
+  * **Big-endian:** `12 34 56 78`
+  * **Little-endian:** `78 56 34 12`
+
+  Examples of big-endian architectures include older Motorola processors (e.g., the 68000 series) and some network protocols that follow network byte order (which is big-endian).
+
+* Since PNG files require data to be written in big-endian format, and many systems natively use little-endian, we need to **reverse the byte order** of multi-byte integers when writing to (or reading from) a PNG file on a little-endian machine. This ensures proper interpretation and compatibility with the PNG specification.
+
+
+
 ### References:
 
 * [PNG Signature - PNG Datastream Structure](https://www.w3.org/TR/2003/REC-PNG-20031110/#5PNG-file-signature)
--- png.c
+++ png.c
@@ -2,35 +2,72 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
 
 #define PNG_SIG_CAP 8
 const uint8_t png_sig[PNG_SIG_CAP] = {137, 80, 78, 71, 13, 10, 26, 10};
+#define CHUNK_BUF_CAP (32 * 1024)
+uint8_t chunk_buf[CHUNK_BUF_CAP];
 
-void read_bytes_or_panic(FILE *file, uint8_t *buf, size_t buf_cap) {
+#define INJECTED_DATA "Hello world, from @realsanjeev"
+#define INJECTED_SIZE sizeof(INJECTED_DATA)
+
+
+#define read_bytes_or_panic(file, buf, buf_cap) read_bytes_or_panic_(file, buf, buf_cap, __FILE__, __LINE__)
+void read_bytes_or_panic_(FILE *file, void *buf, size_t buf_cap, const char *source_file, int source_line) {
     size_t n = fread(buf, buf_cap, 1, file);
     if (n != 1) {
         if (ferror(file)) {
-            fprintf(stderr, "ERROR: could not read PNG header: %s\n",
-                    strerror(errno));
+            fprintf(stderr, "%s:%d: ERROR: could not read %zu bytes from file: %s\n",
+                    source_file, source_line,
+                    buf_cap, strerror(errno));
             exit(1);
         } else if (feof(file)) {
-            fprintf(stderr, "ERROR: could not read PNG header: reached the end of file\n");
+            fprintf(stderr, "%s:%d: ERROR: could not read %zu bytes from file: reached the end of file\n",
+                    source_file, source_line,
+                    buf_cap);
             exit(1);
         } else {
             assert(0 && "Unreachable");
         }
     }
-
 }
 
+#define write_bytes_or_panic(file, buf, buf_cap) write_bytes_or_panic_(file, buf, buf_cap, __FILE__, __LINE__)
+void write_bytes_or_panic_(FILE *file, void *buf, size_t buf_cap, const char *source_file, int source_line) {
+    size_t n = fwrite(buf, buf_cap, 1, file);
+    if (n != 1) {
+        if (ferror(file)) {
+            fprintf(stderr, "%s:%d: ERROR: could not write %zu bytes to file: %s\n",
+                    source_file, source_line,
+                    buf_cap, strerror(errno));
+            exit(1);
+        } else {
+            assert(0 && "Unreachable");
+        }
+    }
+}
 
 void print_bytes(uint8_t *buf, size_t buf_cap) {
-    for (size_t i = 0;  i < buf_cap; ++i) {
+    for (size_t i = 0; i < buf_cap; ++i) {
         printf("%u ", buf[i]);
     }
     printf("\n");
+}
+
+void reverse_bytes(void *buf0, size_t buf_cap) {
+    uint8_t *buf = buf0;
+    for (size_t i = 0; i < buf_cap/2; ++i) {
+        uint8_t t = buf[i];
+        buf[i] = buf[buf_cap - i - 1];
+        buf[buf_cap - i - 1] = t;
+    }
+}
+
+void usage(FILE *file, char *program) {
+    fprintf(file, "Usage: %s <input.png> <output.png>\n", program);
 }
 
 int main(int argc, char **argv) {
@@ -39,12 +76,18 @@ int main(int argc, char **argv) {
     char *program = *argv++;
 
     if (*argv == NULL) {
-        fprintf(stderr, "Usage: %s <input.png>\n", program);
+        usage(stderr, program);
         fprintf(stderr, "ERROR: no input file is provided\n");
         exit(1);
     }
-
     char *input_filepath = *argv++;
+
+    if (*argv == NULL) {
+        usage(stderr, program);
+        fprintf(stderr, "ERROR: no output file is provided\n");
+        exit(1);
+    }
+    char *output_filepath = *argv++;
 
     FILE *input_file = fopen(input_filepath, "rb");
     if (input_file == NULL) {
@@ -53,17 +96,78 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    uint8_t sig[8];
+    FILE *output_file = fopen(output_filepath, "wb");
+    if (output_file == NULL) {
+        fprintf(stderr, "ERROR: could not open file %s: %s\n",
+                output_filepath, strerror(errno));
+        exit(1);
+    }
+
+    uint8_t sig[PNG_SIG_CAP];
     read_bytes_or_panic(input_file, sig, PNG_SIG_CAP);
+    write_bytes_or_panic(output_file, sig, PNG_SIG_CAP);
+    printf("Signature: ");
     print_bytes(sig, PNG_SIG_CAP);
     if (memcmp(sig, png_sig, PNG_SIG_CAP) != 0) {
-        fprintf(stderr, "ERROR: %s does nnot appear to be a valid PNG imaage\n",
+        fprintf(stderr, "ERROR: %s does not appear to be a valid PNG image\n",
                 input_filepath);
         exit(1);
     }
 
-    printf("%s is a valid PNG image\n", input_filepath);
+    bool quit = false;
+    while (!quit) {
+        uint32_t chunk_sz;
+        read_bytes_or_panic(input_file, &chunk_sz, sizeof(chunk_sz));
+        write_bytes_or_panic(output_file, &chunk_sz, sizeof(chunk_sz));
+        reverse_bytes(&chunk_sz, sizeof(chunk_sz));
+
+        uint8_t chunk_type[4];
+        read_bytes_or_panic(input_file, chunk_type, sizeof(chunk_type));
+        write_bytes_or_panic(output_file, chunk_type, sizeof(chunk_type));
+
+        if (*(uint32_t*)chunk_type == 0x444E4549) {
+            quit = true;
+        }
+
+        size_t n = chunk_sz;
+        while (n > 0) {
+            size_t m = n;
+            if (m > CHUNK_BUF_CAP) {
+                m = CHUNK_BUF_CAP;
+            }
+            read_bytes_or_panic(input_file, chunk_buf, m);
+            write_bytes_or_panic(output_file, chunk_buf, m);
+            n -= m;
+        }
+
+        uint32_t chunk_crc;
+        read_bytes_or_panic(input_file, &chunk_crc, sizeof(chunk_crc));
+        write_bytes_or_panic(output_file, &chunk_crc, sizeof(chunk_crc));
+
+        if (*(uint32_t*)chunk_type == 0x52444849) {
+            uint32_t injected_sz = INJECTED_SIZE;
+            reverse_bytes(&injected_sz, sizeof(injected_sz));
+            write_bytes_or_panic(output_file, &injected_sz, sizeof(injected_sz));
+            reverse_bytes(&injected_sz, sizeof(injected_sz));
+
+            char *injected_type = "faKE";
+            write_bytes_or_panic(output_file, injected_type, 4);
+
+            write_bytes_or_panic(output_file, INJECTED_DATA, injected_sz);
+
+            uint32_t injected_crc = 0;
+            write_bytes_or_panic(output_file, &injected_crc, sizeof(injected_crc));
+        }
+
+        printf("Chunk size: %u\n", chunk_sz);
+        printf("Chunk type: %.*s (0x%08X)\n", (int) sizeof(chunk_type), chunk_type, *(uint32_t*) chunk_type);
+        printf("Chunk CRC:  0x%08X\n", chunk_crc);
+        printf("------------------------------\n");
+    }
+    printf("%s has encrypted data\n", output_filepath);
 
     fclose(input_file);
+    fclose(output_file);
+
     return 0;
 }
